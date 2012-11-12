a9a86f85c3cb12bb48efd263d3687970e02a775a
Mateusz Szulc <mateusz.szulc@gmail.com>
2012-11-12 22:13:24 +0100

    Implemented example hotkey.
    Source for HotKey.cs:
    http://bloggablea.wordpress.com/2007/05/01/global-hotkeys-with-net/

--- DictionaryPlatform/MainWindow.cs
+++ DictionaryPlatform/MainWindow.cs
@@ -12,11 +12,34 @@ namespace DictionaryPlatform
     public partial class MainWindow : Form
     {
         ClipboardMonitor cm;
+        Hotkey hk;
         public MainWindow()
         {
             InitializeComponent();
             cm = new ClipboardMonitor();
             cm.ClipboardChanged += cm_ClipboardChanged;
+
+
+
+            hk = new Hotkey();
+            hk.KeyCode = Keys.D1;
+            hk.Windows = false;
+            hk.Control = true;
+            hk.Pressed += delegate { Console.WriteLine("Windows+1 pressed!"); };
+
+            if (!hk.GetCanRegister(this))
+            {
+                Console.WriteLine("Whoops, looks like attempts to register will fail or throw an exception, show an error/visual user feedback");
+            }
+            else
+            {
+                hk.Register(this);
+            }
+
+            //// .. later, at some point
+            //if (hk.Registered)
+            //{ hk.Unregister(); }
+
         }
 
         void cm_ClipboardChanged(object sender, ClipboardChangedEventArgs e)
